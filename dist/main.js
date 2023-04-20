19f07766ece820858823229ba7bc928276b9b1ee
Alec Robertson <4457688+alecrobertson1@users.noreply.github.com>
2023-04-20 17:32:54 +0100

    Initial commit

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_mailjet_1 = require("node-mailjet");
+const messageState = node_mailjet_1.Message.MessageState.BadOrEmptyTemplate;
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,3 @@
+import { Message } from "node-mailjet";
+
+const messageState: Message.MessageState = Message.MessageState.BadOrEmptyTemplate
